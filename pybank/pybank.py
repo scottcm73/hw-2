ab38f9615804c0b30f89d4ecb825e7945a5ab967
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2019-11-21 15:01:00 -0600

    First version of pybank

--- pybank/pybank.py
+++ pybank/pybank.py
@@ -3,8 +3,12 @@ from pathlib import Path
 
 
 
-def to_date_profit_or_loss(porl_month, porl_ammount ):
-    return
+def to_date_profit_or_loss(the_dates, porl):
+    todatesum=0
+   
+    todatesum=sum(porl)
+    
+    return todatesum
 
 def profit_monthly_change():
     return
@@ -21,14 +25,29 @@ data_folder = Path("pybank/resources/")
 file_to_open = data_folder / "budget_data2.csv"
 
 with open(file_to_open, "r") as this_csv_file:
-    this_csv_reader=csv.DictReader(this_csv_file)
+    this_csv_reader=csv.reader(this_csv_file)
     row_count=0
-    for row in this_csv_reader:
-        print (row.keys())
+    the_dates=[]
+    porl=[]
+    todatepl=0
+    this_to_date_p_or_l=[]
+    header=next(this_csv_reader)
+    previous_row=0
+    for row in this_csv_reader:       
         row_count +=1
-        row['Date'], row['Profit/Loss']
+        the_dates.append(row[0])
+        porl.append(float(row[1]))
+        todatepl=to_date_profit_or_loss(the_dates, porl)
+        previous_row=row
+       
+
+        
+
+
 
 
 
 
     print(row_count)
+
+    print(this_to_date_p_or_l)
