65e0745f1f2f7c1283a9bda6c194db40fdd69584
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2019-11-22 11:18:24 -0600

    pypoll3 with error in candidate_list function.

--- pypoll/pypoll3.py
+++ pypoll/pypoll3.py
@@ -4,35 +4,52 @@ vote_tally=0
 vote_tally_by_county=0
 counties=[]
 total_votes=0
-candidates=[]
+candidates=[""]
 class Vote:
     total_votes=total_votes+1
+
 
     def _init_(self, voter_id, county, vote_for_candidate):
         self.voter_id=voter_id
         self.county=county
         self.vote_for_candidate=vote_for_candidate
     
+    def running_vote_tally_by_candidate(self, candidates, vote_tally):
+        # Gets the integer of the vote count of the particular 
+        # candidate in the candidates list with the same index as the current candidate
+        for i in range(len(candidates)):
+            candidate_index=i
+            vote_int=vote_tally[candidate_index]
+            vote_int=vote_int+1
+            # sets the vote_counter list at that same candidate index equal to the vote_int
+            vote_tally[candidate_index]=vote_int
+
+        return vote_tally
+
+    def running_tally_by_candidate_and_county(self):
+        return vote_tally_by_county
+
+    
+    
     def candidate_list(self, this_candidate, candidates):
         if this_candidate not in candidates:
             # Makes a list of only distinct counties
             candidates.append(this_candidate)
-        return
+        
+        else: 
+            running_vote_tally_by_candidate(candidates, vote_tally)
+        return candidates
+    
+    
 
     def county_list(self, this_county, counties):
         if this_county not in counties:
             # Makes a list of only distinct counties
             counties.append(this_county)
-        return
+        return counties
 
-    def running_vote_tally_by_candidate(self):
-        
-
-        return vote_tally
     
-    def running_tally_by_candidate_and_county(self):
-        
-        return vote_tally_by_county
+    
 
 
    
