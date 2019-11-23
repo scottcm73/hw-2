63a47146d3ab071daba80d5ffeade92df9bf9be4
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2019-11-23 09:20:12 -0600

    pybank.py

--- pybank/pybank.py
+++ pybank/pybank.py
@@ -10,8 +10,8 @@ def to_date_profit_or_loss(the_dates, porl):
     
     return todatesum
 
-def profit_monthly_change():
-    return
+def profit_monthly_change(this_to_date_p_or_l, row_before):
+    return this_to_date_p_or_l[row_before+1]-this_to_date_p_or_l[row_before]
 
 def average_monthly_change():
     return
@@ -19,26 +19,32 @@ def average_monthly_change():
 def total_profit_or_loss():
     return
 
+row_count=0
+the_dates=[]
+porl=[]
+todatepl=0
+this_to_date_p_or_l=[]
+profit_change=[]
+previous_row=0
+this_profit_change=0
 
+#Uses Path method of pathlib object to make paths cross platform compatible 
 data_folder = Path("pybank/resources/")
 
 file_to_open = data_folder / "budget_data2.csv"
 
 with open(file_to_open, "r") as this_csv_file:
-    this_csv_reader=csv.reader(this_csv_file)
-    row_count=0
-    the_dates=[]
-    porl=[]
-    todatepl=0
-    this_to_date_p_or_l=[]
+    this_csv_reader=csv.reader(this_csv_file, delimiter=",")
     header=next(this_csv_reader)
-    previous_row=0
     for row in this_csv_reader:       
         row_count +=1
         the_dates.append(row[0])
-        porl.append(float(row[1]))
-        todatepl=to_date_profit_or_loss(the_dates, porl)
+        #makes list of profit or loss (porl) amounts by month
+        porl.append(int(row[1]))
+        this_to_date_p_or_l.append(to_date_profit_or_loss(the_dates, porl))
+        
         previous_row=row
+        
        
 
         
@@ -51,3 +57,5 @@ with open(file_to_open, "r") as this_csv_file:
     print(row_count)
 
     print(this_to_date_p_or_l)
+
+   
