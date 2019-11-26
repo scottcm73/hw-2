572d8eb53c77928d55c0812dc3ec61f1e8edded7
Scottdude73 <53838044+scottcm73@users.noreply.github.com>
2019-11-26 09:04:23 -0600

    Cleaned up print for pyparagraph.py

--- extra_credit/pyparagraph.py
+++ extra_credit/pyparagraph.py
@@ -2,9 +2,7 @@
 import os
 
 
-
-
-file_to_open = os.path.join("raw_data", "paragraph_1.txt")
+file_to_open = os.path.join("raw_data", "paragraph_2.txt")
 num_words=0
 num_sentences=0
 
@@ -15,10 +13,6 @@ print(this_text)
 
 this_text2=this_text
 
-
-
-
-print(this_text2)
 words=this_text2.split()
 total_char=0
 for word in words:
@@ -27,20 +21,22 @@ for word in words:
 
 average_word_length=total_char/num_words
 
+#Assumes that ., ?, and ! end sentences.
+#Unfortunately it also counts abbreviations ending with a period.
 for char in "!?.":
     num_sentences=this_text2.count(char)
 
 for char in "!.->\n":
     this_text2=this_text2.replace(char, " ") 
 
-print(words)
-
 average_sentence_length=num_words/num_sentences
 
-
-print("Number of words: " + str(num_words))  
+print("--------------------------------------")
+print("Paragraph Analysis")
+print("--------------------------------------")
+print("Approximate number of words: " + str(num_words))  
 print("Average word length: %2.1f"%(average_word_length))
-print("Number of sentences: " + str(num_sentences))
-print("Average sentence length in words: " + str(average_sentence_length))
-
+print("Approximate number of sentences: " + str(num_sentences))
+print("Average sentence length in words: %2.1f"%(average_sentence_length))
+print("--------------------------------------")
 
